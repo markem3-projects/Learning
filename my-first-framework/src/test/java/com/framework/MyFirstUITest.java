71824532daeda1c2688a19343a568d951215bd27
Marianna <marianna.kempiak@gmail.com>
2022-08-02 11:54:04 +0200

    added first tests

---
+++ my-first-framework/src/test/java/com/framework/MyFirstUITest.java
@@ -0,0 +1,15 @@
+package com.framework;
+
+import org.junit.jupiter.api.Test;
+
+public class MyFirstUITest {
+
+    @Test
+    void userNameIsCorrectOnOverviewTab() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
